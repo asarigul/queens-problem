ab2d4ac0bb711efd468809920a637f2b8f6a1549
akin.sarigul@gmail.com <akin.sarigul@gmail.com>
2024-09-06 17:22:52 -0500

    rf

--- NQueensSolver.java
+++ NQueensSolver.java
@@ -8,52 +8,66 @@ public class NQueensSolver {
     private final Board board;
     private final List<Board> result = new ArrayList<>();
 
-    // put Queen in each column. don't select the same columns
+    // previously used columns
     private Set<Integer> columns = new HashSet<>();
-    // don't try the cells on the same left (-) diagonals
-    private Set<Integer> leftDiagonals = new HashSet<>();
-    // don't try the cells on the same right (+) diagonals
+
+    // previously used diagonals
     private Set<Integer> rightDiagonals = new HashSet<>();
+    private Set<Integer> leftDiagonals = new HashSet<>();
 
     private NQueensSolver(int boardSize) {
         this.board = new Board(boardSize);
     }
 
     private List<Board> solve() {
-        backtrack(0);
+        tryRow(0);
         return result;
-    } 
+    }
 
-    private void backtrack(int row) {
-        // trying Queen in the last row => a valid solution
-        if(row == this.board.size()) {
+    private void tryRow(int row) {
+        // tried Queen in the last row => a valid solution
+        if (row == this.board.size()) {
             result.add(board.copy());
             return;
         }
 
-        for(int column = 0; column < board.size(); column++) {
-            // skip if on the same column, or on the same diagonals
-            if (columns.contains(column) || rightDiagonals.contains(row + column)
-                    || leftDiagonals.contains(row - column)) {
+        for (int column = 0; column < board.size(); column++) {
+            // skip if this column used before or on a diagonal used before
+            if (isOnTheSameColumnOrDiagonal(row, column)) {
                 continue;
             }
 
-            columns.add(column);
-            leftDiagonals.add(row + column);
-            rightDiagonals.add(row - column);
+            // put Queen here
+            tryThisCell(row, column);
 
-            // put Q here
-            board.putQueen(row, column);
+            // recursively try next row
+            tryRow(row + 1);
 
-            // try next row
-            backtrack(row + 1);
-
-            // didn't work => remove 
-            columns.remove(column);
-            leftDiagonals.remove(row + column);
-            rightDiagonals.remove(row - column);
-            board.removeQueen(row, column);
+            // didn't work => remove
+            undo(row, column);
         }
+    }
+
+    private boolean isOnTheSameColumnOrDiagonal(int row, int column) {
+        return columns.contains(column) || //
+                rightDiagonals.contains(row + column) || //
+                leftDiagonals.contains(row - column);
+    }
+
+    private void tryThisCell(int row, int column) {
+        columns.add(column);
+        rightDiagonals.add(row + column);
+        leftDiagonals.add(row - column);
+
+        board.putQueen(row, column);
+    }
+
+    private void undo(int row, int column) {
+        columns.remove(column);
+        rightDiagonals.remove(row + column);
+        leftDiagonals.remove(row - column);
+
+        board.removeQueen(row, column);
     }
 
     public static void main(String[] args) {
